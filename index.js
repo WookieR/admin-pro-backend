8481e87b12c21487efd72018d09c21ab8c717317
WookieR <matiasroldanrma@gmail.com>
2020-09-24 18:53:40 -0300

    SPA

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
+const path = require('path');
 
 const { dbConnection } = require('./database/config')
 
@@ -27,6 +28,11 @@ app.use('/api/hospital', require('./routes/hospitales'));
 app.use('/api/medico', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/upload'));
+
+//LO ULTIMO
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto: ' + process.env.PORT);
